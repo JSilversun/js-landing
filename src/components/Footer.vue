396a9b3b5022cae0c237bb1fa7d06022751ab545
Julia Suárez <js@plusteam.tech>
2021-04-11 16:38:28 -0400

    refactor: ⛄️ Refactor footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,7 @@
 <template>
   <v-container fluid class="pa-10 grey darken-4 text-center">
     <p class="mb-0">
-      Made with <v-icon>$mdi-heart</v-icon> by Jsilversun with VueJS +
-      Vuetify
+      Made with <v-icon>$mdi-heart</v-icon> by Jsilversun with VueJS + Vuetify
     </p>
   </v-container>
 </template>
@@ -11,6 +10,5 @@ import { defineComponent } from "@vue/composition-api";
 
 export default defineComponent({
   name: "MainFooter",
-  data() {},
 });
 </script>
